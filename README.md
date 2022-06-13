13bf2cf30e7f5b02d450542db444bfba5d0fdfff
Abos2024 <abos2024@gmail.com>
2022-06-13 23:32:57 +0300

    Additions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# pixelbyte
+# PixelByte NFT Corporation
+
+This is the official website source code of PixelByte NFT Corp. We expect the best when it comes to just about anything. We don't back down from innovation of greater standards.
---
+++ data/Scripts/scroll_nav.js
@@ -0,0 +1,35 @@
+window.onscroll = () => {scrollNavbar()};
+
+scrollNavbar = () => {
+    // Target elements
+    const navBar = document.getElementById("navbar");
+  const links = document.querySelectorAll("#navbar .active");
+  const text = document.querySelectorAll("#navbar .topnav-text");
+
+  if (document.documentElement.scrollTop > 50) {
+    navBar.classList.add("fixed-header");
+
+    // Change the color of links on scroll
+    for (let i = 0; i < links.length; i++) {
+        const element = links[i];
+        element.classList.add('scrolled-active');
+    }
+    for (let i = 0; i < text.length; i++) {
+        const element = text[i];
+        element.classList.add('topnav-text-active');
+    }
+
+  } else {
+    navBar.classList.remove("fixed-header");
+    
+    // Change the color of links back to default
+    for (let i = 0; i < links.length; i++) {
+        const element = links[i];
+        element.classList.remove('scrolled-active');
+    }
+    for (let i = 0; i < text.length; i++) {
+        const element = text[i];
+        element.classList.remove('topnav-text-active');
+    }
+  }
+}
