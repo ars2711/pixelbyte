c508175e6573239c4dddfcea67411c655e299859
Abos2024 <abos2024@gmail.com>
2022-06-16 20:42:39 +0300

    Fixing basic bugs

--- public/data/Scripts/scroll_nav.js
+++ public/data/Scripts/scroll_nav.js
@@ -10,7 +10,6 @@ scrollNavbar = () => {
   if (document.documentElement.scrollTop > 50) {
     navBar.classList.add("fixed-header");
 
-    // Change the color of links on scroll
     for (let i = 0; i < links.length; i++) {
         const element = links[i];
         element.classList.add('scrolled-active');
@@ -35,6 +34,10 @@ scrollNavbar = () => {
     for (let i = 0; i < text.length; i++) {
         const element = text[i];
         element.classList.remove('topnav-text-active');
+    }
+    for (let i = 0; i < logo.length; i++) {
+        const element = logo[i];
+        element.classList.remove('topnav-logo-active');
     }
   }
 }
