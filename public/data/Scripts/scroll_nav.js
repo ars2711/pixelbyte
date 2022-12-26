901665f3fb9ab2680aa7928d670ab09bfe4c7221
Abos2024 <abos2024@gmail.com>
2022-12-27 00:04:26 +0300

    Update scroll_nav.js

--- public/data/Scripts/scroll_nav.js
+++ public/data/Scripts/scroll_nav.js
@@ -1,43 +1,34 @@
-window.onscroll = () => {scrollNavbar()};
+window.onscroll = () => {
+  scrollNavbar();
+};
 
 scrollNavbar = () => {
-    // Target elements
+  // Target elements
   var navBar = document.getElementById("navbar");
   const links = document.querySelectorAll("#navbar .active");
-  const text = document.querySelectorAll("#navbar .topnav-text");
-  const logo = document.querySelectorAll("#navbar .nav-logo");
 
   if (document.documentElement.scrollTop > 50) {
     navBar.classList.add("fixed-header");
 
     for (let i = 0; i < links.length; i++) {
-        const element = links[i];
-        element.classList.add('scrolled-active');
+      const element = links[i];
+      element.classList.add("scrolled-active");
     }
     for (let i = 0; i < text.length; i++) {
-        const element = text[i];
-        element.classList.add('topnav-text-active');
+      const element = text[i];
+      element.classList.add("topnav-text-active");
     }
-    for (let i = 0; i < logo.length; i++) {
-        const element = logo[i];
-        element.classList.add('topnav-logo-active');
-    }
-
   } else {
     navBar.classList.remove("fixed-header");
-    
+
     // Change the color of links back to default
     for (let i = 0; i < links.length; i++) {
-        const element = links[i];
-        element.classList.remove('scrolled-active');
+      const element = links[i];
+      element.classList.remove("scrolled-active");
     }
     for (let i = 0; i < text.length; i++) {
-        const element = text[i];
-        element.classList.remove('topnav-text-active');
-    }
-    for (let i = 0; i < logo.length; i++) {
-        const element = logo[i];
-        element.classList.remove('topnav-logo-active');
+      const element = text[i];
+      element.classList.remove("topnav-text-active");
     }
   }
-}
+};
